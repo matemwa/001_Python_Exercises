e319e5e42c47d2b08f97bd228c40cb429de52ede
matemwa <45270694+matemwa@users.noreply.github.com>
2018-11-22 20:56:51 +0100

    Add files via upload

---
+++ survey.py
@@ -0,0 +1,21 @@
+class AnonymousSurvey():
+    """collect anoonyomus answers to a survey questiob"""
+
+    def __init__(self, question):
+        """store a question, [re[are fpr responses storage"""
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        """shows the survey question"""
+        print(self.question)
+
+    def store_response(self, new_response):
+        """store a single response to a survey"""
+        self.responses.append(new_response)
+
+    def show_results(self):
+        """show all the responses"""
+        print("Survey results:")
+        for response in self.responses:
+            print("\t- " + response)
